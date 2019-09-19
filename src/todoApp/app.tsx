92df52471c4242b4c9e36bbda283e7508cb495bf
Apollo Tang <tang.apollo@gmail.com>
2019-09-19 05:51:59 -0400

    wip

--- src/todoApp/app.tsx
+++ src/todoApp/app.tsx
@@ -13,12 +13,31 @@ interface Todo {
 
 type Todos = Todo[]
 
+interface TodosProps {
+  todo: Todo
+  deleteTodo: ()=>void
+  toggleTodo: ()=>void
+}
+
+const TodoItem = ({todo, deleteTodo, toggleTodo}:TodosProps) => {
+  return (
+    <div>
+      <span onClick={deleteTodo} >{todo.text}</span>
+      <button onClick={toggleTodo}>delete</button>
+    </div>
+  )
+}
+
 const App = () => {
   const [todos, setTodos] = useState<Todos>([])
+  const [todoInputText, setTodoInputText] = useState<string>('')
+
   const handle_newTodoChange = (e:React.FormEvent) => {
-    const newTodo = (e.target as HTMLInputElement).value
-    console.log('change', newTodo)
+    e.preventDefault()
+    const inputText = (e.target as HTMLInputElement).value
+    setTodoInputText(inputText)
   }
+
   const handle_newTodoSubmit = (e:React.FormEvent):void => {
     e.preventDefault()
     const formElements =  (e.target as HTMLFormElement).elements as ExtendedHTMLFormElement
@@ -32,19 +51,44 @@ const App = () => {
 
     const nextTodos = [...todos, newTodo]
     setTodos(nextTodos)
+    setTodoInputText('')
   }
-  console.log('todos', todos)
+
+  const handle_delete = (id:string) => () => {
+    console.log(id)
+  }
+
+  const handle_toggle = (id:string) => () => {
+    console.log(id)
+  }
+
   return (
     <div>
       <div>
         <form onSubmit={handle_newTodoSubmit}>
           <label>Add todo:</label>
-          <input type="text" onChange={handle_newTodoChange} name="new-todo" />
+          <input
+            type="text"
+            onChange={handle_newTodoChange}
+            name="new-todo"
+            value={todoInputText}
+          />
         </form>
       </div>
       <div>UI control</div>
       <div>
-        {todos.map(todo=><div key={todo.id}>{todo.text}</div>)}
+        {
+          todos.map(
+            todo=>(
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                deleteTodo={handle_delete(todo.id)}
+                toggleTodo={handle_toggle(todo.id)}
+              />
+            )
+          )
+        }
       </div>
     </div>
   )
