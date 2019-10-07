3673532d3796910a56366ae127d33340065659b4
Apollo Tang <tang.apollo@gmail.com>
2019-10-07 02:10:03 -0400

    [redux-router] illustrate withRouter, update route with new api

--- src/redux-router/app.tsx
+++ src/redux-router/app.tsx
@@ -1,34 +1,55 @@
 import React from 'react'
+
+import {History, Location} from 'history'
 import {
-  BrowserRouter as Router,
+  ConnectedRouter,
+
+} from 'connected-react-router'
+import {
   Route,
-  Link, Switch
-}from 'react-router-dom'
-import {ConnectedRouter} from 'connected-react-router'
-import {History} from 'history'
+  Link,
+  Switch,
+  withRouter,
+} from 'react-router-dom'
+
+
+
+interface TLocationProps {
+  location: Location
+}
+const ShowLocation = ({location}:TLocationProps) => ( <div>Current pathname: {location.pathname}</div> )
+const PathName = withRouter(ShowLocation) // <-- withRouter passes history.location to the wrap component
+
 
 interface TConnectedRouterAppProps {
   history: History
 }
 
+const style = {outline: 'solid 1px black', padding: '5px'}
+
 const ConnectedRouterApp = ({history}:TConnectedRouterAppProps) => (
-  <ConnectedRouter history={history}>
-    <div style={{outline: 'solid 1px black', padding: '5px'}}>
-      <div>Test router:</div>
-      <ul>
-        <li><Link to="/">Link to: /</Link></li>
-        <li><Link to="/a">Link to: /page a</Link></li>
-        <li><Link to="/does-not-exit">Link to: /does-not-exist</Link></li>
-      </ul>
-      <div>
-        <Switch>
-          <Route exact path="/"  component={()=><div>home</div>} />
-          <Route exact path="/a" component={()=><div>Page a</div>} />
-          <Route                 component={()=><div>No match</div>} />
-        </Switch>
-      </div>
-    </div>
-  </ConnectedRouter>
+  <div style={style}>
+    <ConnectedRouter history={history}>
+        <div>Test router:</div>
+        <ul>
+          <li><Link to="/">Link to: /</Link></li>
+          <li><Link to="/a">Link to: /page a</Link></li>
+          <li><Link to="/does-not-exit">Link to: /does-not-exist</Link></li>
+        </ul>
+        <div>
+          <Switch>
+            <Route exact path="/"  component={()=><div>home</div>} />
+            <Route exact path="/a">
+              <div>Page a</div>
+            </Route>
+            <Route>
+              <div>No match</div>
+            </Route>
+          </Switch>
+        </div>
+      <PathName/>
+    </ConnectedRouter>
+  </div>
 )
 
 export default ConnectedRouterApp
