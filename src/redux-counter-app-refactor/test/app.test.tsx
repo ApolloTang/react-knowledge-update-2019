6dc023ff2751db54e9118c4a56a433ab855f616f
Apollo Tang <tang.apollo@gmail.com>
2019-10-13 21:48:52 -0400

    [redux-counter|refactor] add test for decrement

--- src/redux-counter-app-refactor/test/app.test.tsx
+++ src/redux-counter-app-refactor/test/app.test.tsx
@@ -42,3 +42,21 @@ test('can increase counter from default state', ()=>{
   fireEvent.click(increaseButton)
   expect(counterDisplay).toHaveTextContent('1')
 })
+
+
+test('can decrease counter from default state', ()=>{
+  const store = createStore(reducer)
+  const {
+    getByText,
+    getByTestId
+  } = render(
+    <Provider store={store} >
+      <ConnectedApp />
+    </Provider>
+  )
+  const counterDisplay = getByTestId('count-value')
+  const increaseButton = getByText('-')
+  expect(counterDisplay).toHaveTextContent('0')
+  fireEvent.click(increaseButton)
+  expect(counterDisplay).toHaveTextContent('-1')
+})
