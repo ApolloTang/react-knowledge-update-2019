dde3a67f6327883b6b154577f9f6633777ee0252
Apollo Tang <tang.apollo@gmail.com>
2019-10-13 22:35:41 -0400

    [redux-todo-app|refactor] remove default export from store

--- src/redux-todo-app/index.tsx
+++ src/redux-todo-app/index.tsx
@@ -15,7 +15,7 @@ declare global {
 
 
 import {Provider} from 'react-redux'
-import store from './store'
+import {store} from './store'
 
 import App from './app'
 
--- src/redux-todo-app/store.ts
+++ src/redux-todo-app/store.ts
@@ -18,8 +18,8 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-export default store
 export {
+  store,
   Tstore,
   rootReducer
 }
